95595fdfeca7c7312359e0d9bad92f7f738cba0b
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-11-11 19:14:01 -0500

    started adding home routes. Add route for getting all posts

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,9 +1,39 @@
 const router = require('express').Router();
-router.get('/login', (req, res) => {
+const { User, Forum, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//Route used to get all posts
+router.get('/posts', withAuth, async (req, res) => {
+  try {
+    const dbPosts = await Forum.findAll({
+      include: [
+        {
+          mode: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = dbPosts.map((post) => post.get({ plain: true }));
+    res.render('posts', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  res.render('post');
+});
+
+router.get('/login', async (req, res) => {
   res.render('login');
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', async (req, res) => {
   res.render('register');
 });
 module.exports = router;
