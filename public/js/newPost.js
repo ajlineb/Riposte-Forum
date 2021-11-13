cad368d5a81abb70a3df6c98175c915e21902abc
Philip Browning <philip741@gmail.com>
2021-11-13 09:30:20 -0500

    Added functionality to post content to database and show up on the all posts page

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,22 +1,23 @@
-async function submitNewPost(event) {
-    event.preventDefault();
-    const post_title = document.querySelector('#post_title').value;
-    const post_content = document.querySelector('#post_content').value;
 
-    const response = await fetch(`/api/forum/`, {
-        method: 'PUT',
+async function submitNewPost(event) {
+    console.log("Running submit post");
+    event.preventDefault();
+    const post_title = document.getElementById('post_title').value;
+    const post_content = document.getElementById('post_content').value;
+
+    const response = await fetch('http://localhost:3001/api/forum', {
+        method: 'POST',
         body: JSON.stringify({
             "forum_name": post_title,
             "forum_text": post_content,
             "forum_time_stamp": "2001-01-03 01:44:00", 
-            "userid": "1" 
+            "userid": 1 
         }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
 }
-
-document
-  .querySelector('#post_button')
-  .addEventListener('submit', submitNewPost);
+const sB = document.getElementById('post_button');
+//console.log(sB)
+sB.addEventListener('click', submitNewPost)
