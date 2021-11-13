22d2d543e173efc368235be4aefdb25b4df1e3c4
Terrence Meikle <terrence_meikle@yahoo.com>
2021-11-13 10:53:51 -0500

    fixing login route

--- public/js/login.js
+++ public/js/login.js
@@ -7,14 +7,14 @@ const loginHandler = async (event) => {
   const password = document.querySelector('#password').value.trim();
 
   if (email && password) {
-    const response = await fetch('/allposts', {
-      method: 'GET',
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('./all-post');
+      document.location.replace('/all-post');
     } else {
       alert('Failed to log in.');
     }
