bc8d03e78765973018251cf6aa6cc6aff156c823
Philip Browning <philip741@gmail.com>
2021-11-15 10:02:00 -0500

    Added function to get user id from session Added that user id to the post data when new post is created

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,9 +1,19 @@
 
+async function getUser(){
+    const response = await fetch('/session');
+    const jsonRes = await response.json();
+    const allUsers = await fetch('/api/user');
+    const allUserJson = await allUsers.json();
+    let matchId = allUserJson.filter(user => user.username === jsonRes.username).map(user => user.id);
+    return matchId
+}
+
+
 async function submitNewPost(event) {
-    console.log("Running submit post");
     event.preventDefault();
     const post_title = document.getElementById('post_title').value;
     const post_content = document.getElementById('post_content').value;
+    const userName = await getUser();
 
     const response = await fetch('/api/forum', {
         method: 'POST',
@@ -11,13 +21,15 @@ async function submitNewPost(event) {
             "forum_name": post_title,
             "forum_text": post_content,
             "forum_time_stamp": "2001-01-03 01:44:00", 
-            "user_id": 1 
+            "user_id": parseInt(userName)
         }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
+      const text = response.status;
 }
-const sB = document.getElementById('post_button');
-//console.log(sB)
-sB.addEventListener('click', submitNewPost)
+const submitPost = document.getElementById('post_button');
+submitPost.addEventListener('click', submitNewPost);
+
+  
