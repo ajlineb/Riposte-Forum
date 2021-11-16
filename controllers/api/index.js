9d02a4289f83d90e75ca8faa073b8330f3b741cb
Philip Browning <philip741@gmail.com>
2021-11-16 11:50:10 -0500

    Added route for email send

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const forumRoutes = require('./forum-routes');
 const commentRoutes = require('./comment-routes');
+const mailRoutes = require('./send-mail');
 
 router.use('/user', userRoutes);
 router.use('/forum', forumRoutes);
 router.use('/comments', commentRoutes);
+router.use('/email', mailRoutes);
 
 module.exports = router;
